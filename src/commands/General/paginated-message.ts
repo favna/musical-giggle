1c464bf3d256c6d44b7769b924c5578c6cfaef66
Jeroen Claassens <support@favware.tech>
2021-10-08 19:31:21 +0200

    chore: setup portalling of djs utils

--- src/commands/General/paginated-message.ts
+++ src/commands/General/paginated-message.ts
@@ -1,9 +1,9 @@
 import { ApplyOptions } from '@sapphire/decorators';
 import { PaginatedMessage } from '@sapphire/discord.js-utilities';
 import { Command, CommandOptions } from '@sapphire/framework';
+import { send } from '@sapphire/plugin-editable-commands';
 import type { Message } from 'discord.js';
 import { MessageEmbed } from 'discord.js';
-import { sendLoadingMessage } from '../../lib/utils';
 
 @ApplyOptions<CommandOptions>({
 	aliases: ['pm'],
@@ -12,7 +12,7 @@ import { sendLoadingMessage } from '../../lib/utils';
 })
 export class UserCommand extends Command {
 	public async run(message: Message) {
-		const response = await sendLoadingMessage(message);
+		const response = await send(message, { content: 'loading...' });
 
 		const paginatedMessage = new PaginatedMessage({
 			template: new MessageEmbed()
