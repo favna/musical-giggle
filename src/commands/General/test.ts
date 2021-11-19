244b590ae011ec56ae2ae2f43590e7eb6b930608
Jeroen Claassens <support@favware.tech>
2021-11-19 20:53:45 +0100

    chore: wiptf
    permalink: http://whatthecommit.com/6a7b38d50a48073408b8078748129b03

--- src/commands/General/test.ts
+++ src/commands/General/test.ts
@@ -1,14 +1,32 @@
 import { ApplyOptions } from '@sapphire/decorators';
-import { Args, Command, CommandOptions } from '@sapphire/framework';
-import type { Message } from 'discord.js';
+import { PaginatedMessage } from '@sapphire/discord.js-utilities';
+import { Command, CommandOptions } from '@sapphire/framework';
+import { Constants, Message, MessageEmbed } from 'discord.js';
 
 @ApplyOptions<CommandOptions>({
 	description: 'ping pong'
 })
 export class UserCommand extends Command {
-	public async messageRun(_message: Message, args: Args) {
-		const result = await args.pickResult('guildCategoryChannel');
+	public async messageRun(message: Message) {
+		const paginationEmbed = new PaginatedMessage({
+			template: new MessageEmbed() //
+				.setColor('BLUE')
+				.setTitle(`Hi`)
+				.setFooter('Hi')
+		});
 
-		return console.log(result);
+		paginationEmbed //
+			.addPageEmbed((embed) => embed.setDescription('Hi'))
+			.addPageEmbed((embed) => embed.setDescription('Hello'));
+
+		paginationEmbed.addAction({
+			type: Constants.MessageComponentTypes.BUTTON,
+			customId: 'Ban',
+			emoji: "🔨",
+			style: 'PRIMARY',
+			run: async () => console.log('hi')
+		});
+
+		return paginationEmbed.run(message, message.author);
 	}
 }
