c2a0f0725bcc51611572f7e34fa4530a9079d38b
Jeroen Claassens <support@favware.tech>
2022-08-21 09:20:57 +0200

    feat: add subcommand with groups

---
+++ src/commands/General/subcommand-groups.ts
@@ -0,0 +1,37 @@
+import { ApplyOptions } from '@sapphire/decorators';
+import { Subcommand } from '@sapphire/plugin-subcommands';
+import type { Message } from 'discord.js';
+
+@ApplyOptions<Subcommand.Options>({
+  aliases: ['sg'],
+  description: 'A basic command with some subcommand groups',
+  subcommands: [
+    {
+      type: 'group',
+      name: 'config',
+      entries: [
+        { name: 'edit', messageRun: 'configEdit' },
+        { name: 'show', messageRun: 'configShow', default: true },
+        { name: 'remove', messageRun: 'configRemove' },
+        { name: 'reset', messageRun: 'configReset' }
+      ]
+    }
+  ]
+})
+export class UserCommand extends Subcommand {
+  public async configShow(message: Message) {
+    return message.channel.send('Showing!');
+  }
+
+  public async configEdit(message: Message) {
+    return message.channel.send('Editing!');
+  }
+
+  public async configRemove(message: Message) {
+    return message.channel.send('Removing!');
+  }
+
+  public async configReset(message: Message) {
+    return message.channel.send('Resetting!');
+  }
+}
